b1495c0b4a6b60f5dcc361df5360b263916e4d89
rolzkie <133796550+rolzkie@users.noreply.github.com>
2026-02-07 12:39:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ noBtn.addEventListener("mouseover", () => {
 yesBtn.addEventListener("click", () => {
     title.textContent = "Yippeeee!";
 
-    catImg.src = "/ASSETS/cat_dance.gif";
+    catImg.src = "cat_dance.gif";
 
     document.querySelector(".letter-window").classList.add("final");
 
